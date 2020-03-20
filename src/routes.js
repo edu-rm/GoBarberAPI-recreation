60f04b7b7391e7e056004a054321fa3158a06bb7
EduardoRamponMeireles <eduardomeireles3232@gmail.com>
2020-03-20 15:39:22 -0300

    sucrase and nodemon installed. It was refactored some code line to fit in sucrase syntax

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,10 @@
-const { Router } = require('express');
+import  Router from 'express';
 
 const routes = new Router();
 
 routes.get('/', (req,res) =>{
+  return res.json({ message : 'hello world'})
 })
 
 
-module.exports = routes; 
+export default routes; 
