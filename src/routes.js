21fc60f9529f918e78d6f657aa3c03209bc50c7b
EduardoRamponMeireles <eduardomeireles3232@gmail.com>
2020-03-24 16:40:21 -0300

    multer middleware for single image upload created

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,6 @@
 import Router from "express";
+import multer from "multer";
+import multerConfig from "./config/multer";
 
 import UserController from "./app/controllers/UserController";
 import SessionController from "./app/controllers/SessionController";
@@ -6,6 +8,7 @@ import SessionController from "./app/controllers/SessionController";
 import authMiddleware from "./app/middlewares/auth";
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post("/users", UserController.store);
 routes.post("/sessions", SessionController.store);
@@ -14,4 +17,7 @@ routes.use(authMiddleware);
 
 routes.put("/users", UserController.update);
 
+routes.post("/files", upload.single("file"), (req, res) => {
+  return res.json({ ok: true });
+});
 export default routes;
