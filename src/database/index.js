a6851cc535ab824a2abf96f8f4356423ffc31136
EduardoRamponMeireles <eduardomeireles3232@gmail.com>
2020-03-26 15:01:10 -0300

    application connected with mongodb

--- src/database/index.js
+++ src/database/index.js
@@ -1,4 +1,5 @@
 import Sequelize from "sequelize";
+import mongoose from "mongoose";
 
 import User from "../app/models/User";
 import File from "../app/models/File";
@@ -11,6 +12,7 @@ const models = [User, File, Appointment];
 class Database {
   constructor() {
     this.init();
+    this.mongo();
   }
 
   init() {
@@ -21,6 +23,13 @@ class Database {
       .map(
         (model) => model.associate && model.associate(this.connection.models)
       );
+  }
+
+  mongo() {
+    this.mongoConnection = mongoose.connect(
+      "mongodb://localhost:27017/barbermanager",
+      { useNewUrlParser: true, useFindAndModify: true }
+    );
   }
 }
 
