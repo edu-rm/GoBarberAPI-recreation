1df634de1ee9846b42e18c6fba0432b44acab686
EduardoRamponMeireles <eduardomeireles3232@gmail.com>
2020-03-26 15:57:57 -0300

    feature: notify provider new appointments

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,8 +1,10 @@
 import * as Yup from "yup";
-import { startOfHour, parseISO, isBefore } from "date-fns";
+import { startOfHour, parseISO, isBefore, format } from "date-fns";
+import pt from "date-fns/locale/pt";
 import User from "../models/User";
 import Appointment from "../models/Appointment";
 import File from "../models/File";
+import Notification from "../schemas/Notification";
 
 class AppointmentController {
   async index(req, res) {
@@ -40,7 +42,7 @@ class AppointmentController {
     });
 
     if (!(await schema.isValid(req.body))) {
-      return res.status(400).json({ error: "Validation fails " });
+      return res.status(400).json({ error: "Validation fails" });
     }
 
     const { provider_id, date } = req.body;
@@ -87,6 +89,22 @@ class AppointmentController {
       user_id: req.userId,
       provider_id,
       date,
+    });
+
+    /**
+     * Notify appointment to provider
+     */
+
+    const user = await User.findByPk(req.userId);
+    const formattedDate = format(
+      hourStart,
+      "'dia' dd 'de' MMMM', às' H:mm'h'",
+      { locale: pt }
+    );
+
+    await Notification.create({
+      content: `Novo agendamento de ${user.name} para ${formattedDate}`,
+      user: provider_id,
     });
 
     return res.json(appointment);
