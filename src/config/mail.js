eb0831e27da8fe43496d09e28f65d0481960ab1e
EduardoRamponMeireles <eduardomeireles3232@gmail.com>
2020-03-28 09:52:11 -0300

    nodemailer configured

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  secure: false,
+  auth: {
+    user: "dc2dc93f19b0fb",
+    pass: "626f6fee0fd3db",
+  },
+  default: {
+    from: "Eduardo Meireles <eduardomeireles3232@gmail.com>",
+  },
+};
